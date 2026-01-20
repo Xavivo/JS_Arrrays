ed9fc7d7ed7ca6b35f33e9ea24a35040cc32484d
AlbertoWebDev <{your_email}>
2026-01-20 09:05:51 +0100

    Ej 2

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const sumaTotal = numeros.reduce((accum, n) => accum + n, 0);
 console.log("Cuadrados:", cuadrados);
 console.log("Pares:", pares);
 console.log("Suma:", sumaTotal);
+
+// EJERCICIO 2: Transformaciones de strings
+const ciudades = ["Skopje","Budapest", "Sevilla", "California", "Toronto"];
+const ciudadesMayus = ciudades.map(c => c.toUpperCase());
+const ciudadesOrdenadas = ciudades.sort();
+const empiezaConM = ciudades.some(c => c.startsWith("M"));
+const todasLargas = ciudades.every(c => c.length > 4);
+
+console.log("Mayúsculas:", ciudadesMayus);
+console.log("Por orden alfabético:", ciudadesOrdenadas);
+console.log("Empieza con M:", empiezaConM);
+console.log("Con 4 letras o más: ", todasLargas);
